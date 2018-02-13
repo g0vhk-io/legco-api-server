d54f4b7dba2187662364ea97005e0852f5b4fc0f
howa <howa.wong@gmail.com>
2018-02-13 22:44:21 +0800

    Add legco models #2

--- api_server/legco/models.py
+++ api_server/legco/models.py
@@ -21,3 +21,92 @@ class Individual(models.Model):
     image = models.CharField(max_length=512, blank=True, null=True, default=None)
     def __unicode__(self):
         return self.name_en + "-" + self.name_ch
+
+class Motion(models.Model):
+    name_en = models.CharField(max_length=512)
+    name_ch = models.CharField(max_length=512)
+    mover_type = models.CharField(max_length=512)
+    mover_ch = models.CharField(max_length=512, default=None, null=True)
+    mover_en = models.CharField(max_length=512, default=None, null=True)
+    mover_individual = models.ForeignKey(Individual, null=True, blank=True)
+    def __str__(self):
+        return self.name_en + "-" + self.name_ch
+
+class ImportantMotion(models.Model):
+    motion = models.ForeignKey(Motion)
+
+
+class Constituency(models.Model):
+    name_en = models.CharField(max_length=512)
+    name_ch = models.CharField(max_length=512)
+    def __str__(self):
+        return self.name_en + "-" + self.name_ch
+
+class Meeting(models.Model):
+    date = models.DateField()
+    meeting_type = models.CharField(max_length=1024,default=None)
+    key = models.CharField(max_length=255, default="no-key", blank=True, null=True, unique=True)
+    source_url = models.CharField(max_length=2048, null=True)
+    def __str__(self):
+        return self.date.strftime('%Y-%m-%d') + "-" + str(self.source_url)
+
+class Vote(models.Model):
+    date = models.DateField()
+    time = models.TimeField()
+    vote_number = models.IntegerField()
+    motion = models.ForeignKey(Motion)
+    meeting = models.ForeignKey(Meeting)
+    def __str__(self):
+        return self.motion.name_en + " " + self.meeting.date.strftime("%Y-%m-%d")
+
+class VoteSummary(models.Model):
+    FUNCTIONAL = 'FUNC'
+    OVERALL = 'OVER'
+    GEOGRAPHICAL = 'GEOG'
+    SUMMARY_CHOICES = ((FUNCTIONAL, 'Functional'),(OVERALL,'Overall'), (GEOGRAPHICAL, 'GEOG'))
+    vote = models.ForeignKey(Vote,default=None)
+    summary_type = models.CharField(max_length=64, choices = SUMMARY_CHOICES)
+    present_count = models.IntegerField(default=0)
+    vote_count = models.IntegerField(default=0)
+    yes_count = models.IntegerField(default=0)
+    no_count = models.IntegerField(default=0)
+    abstain_count = models.IntegerField(default=0)
+    result = models.CharField(max_length=512)
+    def __str__(self):
+        return self.vote.motion.name_en +  " " + self.summary_type + " " + self.result
+
+class IndividualVote(models.Model):
+    individual = models.ForeignKey(Individual)
+    YES = 'YES'
+    NO = 'NO'
+    ABSENT = 'ABSENT'
+    ABSTAIN = 'ABSTAIN'
+    VOTE_CHOICES = ((YES, 'Yes'), (NO, 'No'),(ABSENT, 'Absent'), (ABSTAIN, 'Abstain'))
+    result = models.CharField(max_length=64, choices = VOTE_CHOICES, default=NO)
+    vote = models.ForeignKey(Vote)
+    def __str__(self):
+        return self.vote.motion.name_en + " " + self.individual.name_en + " " + self.result
+
+class MeetingSpeech(models.Model):
+    individual = models.ForeignKey(Individual, null=True, blank=True)
+    others_individual = models.ManyToManyField(Individual, related_name='others')
+    title_ch = models.CharField(max_length=100)
+    text_ch = models.TextField(max_length=33554432, default="")
+    bookmark = models.CharField(max_length=100)
+    sequence_number = models.IntegerField(default=0)
+
+
+class MeetingPersonel(models.Model):
+    individual = models.ForeignKey(Individual, null=True, blank=True)
+    title_ch = models.CharField(max_length=100)
+
+class MeetingHansard(models.Model):
+    date = models.DateField()
+    meeting_type = models.CharField(max_length=128, default="cm")
+    key = models.CharField(max_length=128, unique=True)
+    source_url = models.CharField(max_length=2048)
+    speeches = models.ManyToManyField(MeetingSpeech)
+    members_present = models.ManyToManyField(MeetingPersonel, related_name='present')
+    members_absent = models.ManyToManyField(MeetingPersonel, related_name='absent')
+    public_officers = models.ManyToManyField(MeetingPersonel, related_name='officers')
+    clerks = models.ManyToManyField(MeetingPersonel, related_name='clerks')
