ee72e5c05b4fd78b270f1af5c8c7b8ec5b75ede0
Ho Wa Wong <howa.wong@gmail.com>
2019-07-28 09:56:17 +0800

    Add uwsgi

--- api_server/api_server/settings.py
+++ api_server/api_server/settings.py
@@ -157,4 +157,4 @@ CORS_ORIGIN_ALLOW_ALL = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/app/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
